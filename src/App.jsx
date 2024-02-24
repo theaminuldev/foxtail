db56fca747f490b68408f7cf7291cbdc16add7a3
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-02-25 01:29:30 +0600

    Add FeaturedCollection component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Hero from "./components/blog/Hero";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import FeaturedCollection from "./page/sections/featured collection/FeaturedCollection";
 
 function App() {
 	return (
 		<>
 			<Header />
 			<Hero />
+			<FeaturedCollection />
 			<Footer />
 		</>
 	);
