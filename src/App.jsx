f93c8c03f33f4ebf2567c0ff572b75aa758f88ad
Salman Izhar <Sbinazhar671@gmail.com>
2024-02-24 09:41:03 +0600

    SectionTwo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import "./App.css";
 import Hero from "./components/blog/Hero";
+import SectionTwo from "./components/blog/SectionTwo";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 
 function App() {
-	return (
-		<>
-			<Header />
-			<Hero />
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <Hero />
+      <SectionTwo />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
