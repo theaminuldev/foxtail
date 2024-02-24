1f2cfa8aa8762999fea98d42e6c8365aa3b02a96
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-02-25 01:30:25 +0600

    Add .gitignore entry for pnpm-lock.yaml and create NewsItem component

---
+++ src/page/sections/featured collection/components/NewsItem.jsx
@@ -0,0 +1,26 @@
+const NewsItem = ({ title = "Add news Title", url = "Put news url" }) => {
+  return (
+    <article>
+      <a
+        href={url}
+        className="flex gap-4 p-2 rounded transition-colors duration-200 hover:bg-gray100 hover:shadow-2xl"
+      >
+        <div>
+          <img
+            src="https://via.placeholder.com/126x126"
+            alt="Placeholder"
+            className="max-w-[150px] max-h-[150px] object-cover rounded"
+          />
+        </div>
+        <div className="flex flex-col grow">
+          <h3 className="text-xl font-bold text-gray-800 hover:underline">
+            {title}
+          </h3>
+          <span className=" text-gray-600 underline mt-6">Read More</span>
+        </div>
+      </a>
+    </article>
+  );
+};
+
+export default NewsItem;
