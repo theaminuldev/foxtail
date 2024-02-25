3000860bf3e2c491fb50f2abc10891739b9ac176
theaminuldev <mdaminulislam2015bd@gmail.com>
2024-02-25 18:31:25 +0600

    fix: file and commponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
+import FeaturedCollection from "./components/blog/FeaturedCollection";
 import Hero from "./components/blog/Hero";
 import SectionTwo from "./components/blog/SectionTwo";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
-import FeaturedCollection from "./page/sections/featured collection/FeaturedCollection";
 import { useQueryPosts } from "./hooks";
-
 
 function App() {
 	const postData = useQueryPosts();
 	return (
 		<>
 			<Header />
-			<Hero /
-  		<FeaturedCollection />
+			<Hero />
+			<FeaturedCollection />
 			<SectionTwo />
 			<Footer />
 		</>
